ac73427f9a765b02da2739e13ff16b58bb5285dc
Failsnail <koenmare@gmail.com>
2014-12-30 20:38:34 +0100

    actually includes the vector3D class

---
+++ positionBasedPhysics/vector3d.cpp
@@ -0,0 +1,73 @@
+#include "vector3d.h"
+
+vector3D::vector3D(unit newX, unit newY, unit newZ) {
+    x = newX;
+    y = newY;
+    z = newZ;
+}
+
+vector3D::~vector3D() {
+    delete &x;
+    delete &y;
+    delete &z;
+}
+
+unit vector3D::getLength() const {
+    return (unit)sqrtl(x * x + y * y + z * z);
+}
+
+void vector3D::normalize() {
+    *this = *this / (getLength());
+}
+
+vector3D vector3D::operator+ (const vector3D& otherVector) {
+    return vector3D(x + otherVector.getX(), y + otherVector.getY(), z + otherVector.getZ());
+}
+
+vector3D vector3D::operator- (const vector3D& otherVector) {
+    return vector3D(x - otherVector.getX(), y - otherVector.getY(), z - otherVector.getZ());
+}
+
+vector3D vector3D::operator* (const unit& factor) {
+    return vector3D(x * factor, y * factor, z * factor);
+}
+
+vector3D vector3D::operator/ (const unit& factor) {
+    return operator*(1 / factor);
+}
+
+void vector3D::operator= (const vector3D& newValue) {
+    x = newValue.getX();
+    y = newValue.getY();
+    z = newValue.getZ();
+}
+
+unit vector3D::getX() const {
+    return x;
+}
+
+unit vector3D::getY() const {
+    return y;
+}
+
+unit vector3D::getZ() const {
+    return z;
+}
+
+void vector3D::setX(const unit& newX) {
+    x = newX;
+}
+
+void vector3D::setY(const unit& newY) {
+    y = newY;
+}
+
+void vector3D::setZ(const unit& newZ) {
+    z = newZ;
+}
+
+void vector3D::set(const unit& newX, const unit& newY, const unit& newZ) {
+    x = (unit)newX;
+    y = (unit)newY;
+    z = (unit)newZ;
+}
---
+++ positionBasedPhysics/vector3d.h
@@ -0,0 +1,32 @@
+#ifndef VECTOR3D_H
+#define VECTOR3D_H
+#include <cmath>
+
+using unit = double;
+
+class vector3D {
+    public:
+        vector3D(unit newX = 0, unit newY = 0, unit newZ = 0);
+        ~vector3D();
+        unit getLength() const;
+        unit getX() const;
+        unit getY() const;
+        unit getZ() const;
+        void normalize();
+        void setX(const unit& newX);
+        void setY(const unit& newY);
+        void setZ(const unit& newZ);
+        void set(const unit& newX, const unit& newY, const unit& newZ);
+        vector3D operator+ (const vector3D& otherVector);
+        vector3D operator - (const vector3D& otherVector);
+        vector3D operator * (const unit& factor);
+        vector3D operator / (const unit& factor);
+        void operator = (const vector3D& newValue);
+    protected:
+    private:
+        unit x;
+        unit y;
+        unit z;
+};
+
+#endif // VECTOR3D_H
